2c5c14cdf1890ffd00191274dc9100e161e580e2
Corum Gold <goldcorum@gmail.com>
2022-06-22 15:41:03 -0400

    Finish kth largest

--- kth_largest_element.js
+++ kth_largest_element.js
@@ -10,6 +10,10 @@
 // Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
 // Output: 4
 
+var findKthLargest = function (nums, k) {
+    nums.sort((a, b) => b - a)
+    return nums[k - 1];
+};
 
 findKthLargest([3, 2, 1, 5, 6, 4], 2)
 
