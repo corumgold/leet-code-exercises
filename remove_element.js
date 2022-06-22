b1185d193f01462b8feb0ca645181aa69058114d
Corum Gold <goldcorum@gmail.com>
2022-06-22 18:23:43 -0400

    Solve remove elements

--- remove_element.js
+++ remove_element.js
@@ -41,7 +41,16 @@
 // Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
 // Note that the five elements can be returned in any order.
 // It does not matter what you leave beyond the returned k (hence they are underscores).
- 
-var removeElement = function(nums, val) {
-    
+
+var removeElement = function (nums, val) {
+    debugger;
+    let k = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === val) {
+            nums.splice(i, 1);
+            i--;
+        } else k++
+    } return k;
 };
+
+removeElement([0,1,2,2,3,0,4,2], 2)
