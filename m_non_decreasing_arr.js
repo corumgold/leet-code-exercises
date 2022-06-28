2b7feceda71cf43da7d52c3067d7607573df4207
Corum Gold <goldcorum@gmail.com>
2022-06-28 13:10:59 -0400

    Add length of last word starter file

--- m_non_decreasing_arr.js
+++ m_non_decreasing_arr.js
@@ -13,5 +13,27 @@
 // Explanation: You can't get a non-decreasing array by modify at most one element.
 
 var checkPossibility = function (nums) {
-
-};
+    debugger
+    let oddBird = 0;
+    //look through the array to find the non-decreasing element "oddBird"
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > nums[i - 1]) {
+            oddBird = nums[i];
+            //take oddBird out for the sake of testing
+            nums.splice(i, 1);
+        } else if (i === nums.length - 1) return false;
+    }
+    //loop through again, trying oddBird in each position
+    for (let i = 0; i < nums.length; i++) {
+        nums.splice(i, 0, oddBird);
+        //check if its decreasing order
+        for (let j = 0; j < nums.length; j++) {
+            if (nums[j] < nums[j+1]) {
+                nums.splice(i, 1)
+                break;
+            } else {
+                return true;
+            }
+        }
+    } return false;
+}; checkPossibility([3, 4,2,1])
